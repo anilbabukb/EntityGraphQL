bae149f2fd83290abd018c02d743c2ac66f114a8
Anil Babu <anilbabukb@outlook.com>
2020-03-10 16:32:28 +0400

    Migrated demo project to .NetCore 3.1

--- src/demo/Startup.cs
+++ src/demo/Startup.cs
@@ -9,22 +9,18 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using EntityGraphQL.Schema;
+using Microsoft.Extensions.Hosting;
 
 namespace demo
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+        public Startup(IConfiguration configuration)
+        {            
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -32,21 +28,30 @@ namespace demo
             services.AddDbContext<DemoContext>(opt => opt.UseSqlite("Filename=demo.db"));
 
             // add schema provider so we don't need to create it everytime
-            services.AddSingleton(GraphQLSchema.MakeSchema());
-            services.AddMvc();
+            services.AddSingleton(GraphQLSchema.MakeSchema());            
+            services.AddControllers()
+                .AddNewtonsoftJson();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, DemoContext db)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DemoContext db)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             CreateData(db);
 
-            app.UseFileServer();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-            app.UseMvc();
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
 
         private static void CreateData(DemoContext db)
